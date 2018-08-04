2e8dc723c1def3b3775537968ffb759d3d0bb350
FranciscoDA <franciscoda@outlook.com>
2018-08-03 23:25:07 -0300

    simplify definitions in zipped_range

--- zipped_range.h
+++ zipped_range.h
@@ -3,31 +3,29 @@
 
 #include <tuple>
 
-template<typename Iterator, typename ...Iterators>
+template<typename ...Iterators>
 class zipped_iterator {
-private:
-	using Seq = std::make_index_sequence<sizeof...(Iterators)+1>;
 public:
-	using value_type = std::tuple<typename Iterator::value_type, typename Iterators::value_type...>;
-	using pointer = std::tuple<typename Iterator::pointer, typename Iterators::pointer...>;
-	using reference = std::tuple<typename Iterator::reference, typename Iterators::reference...>;
+	using value_type = std::tuple<typename Iterators::value_type...>;
+	using pointer = std::tuple<typename Iterators::pointer...>;
+	using reference = std::tuple<typename Iterators::reference...>;
 	using iterator_category = std::input_iterator_tag;
-	using difference_type = typename Iterator::difference_type;
+	using difference_type = std::common_type_t<typename Iterators::difference_type...>;
 
 	// initialize the tuple
-	zipped_iterator(Iterator arg1, Iterators... args) : t(arg1, args...) {
+	zipped_iterator(Iterators... args) : t(args...) {
 	}
 
 	// these operators act as a proxy to the private methods
-	zipped_iterator& operator++() { return _prefix_inc(Seq{}); }
-	reference operator*() const { return _deref(Seq{}); }
-	bool operator!=(const zipped_iterator& other) const { return _logical_neq(other, Seq{}); }
+	zipped_iterator& operator++() { return _prefix_inc(std::index_sequence_for<Iterators...>()); }
+	reference operator*() const { return _deref(std::index_sequence_for<Iterators...>()); }
+	bool operator!=(const zipped_iterator& other) const { return _logical_neq(other, std::index_sequence_for<Iterators...>()); }
 
 	template<std::size_t I>
 	auto& get() const { return std::get<I>(t); }
 
 private:
-	std::tuple<Iterator, Iterators...> t;
+	std::tuple<Iterators...> t;
 
 	// use a fold expression to increment all the inner iterators
 	template<std::size_t ...I>
@@ -50,25 +48,24 @@ private:
 	}
 };
 
-template<typename Iterable, typename ...Iterables>
-zipped_iterator<typename Iterable::iterator, typename Iterables::iterator...>
-zipped_begin(Iterable& arg1, Iterables&... args) {
-	return zipped_iterator(std::begin(arg1), std::begin(args)...);
+template<typename ...Iterables>
+auto zipped_begin(Iterables&... args) {
+	return zipped_iterator<typename Iterables::iterator...>(std::begin(args)...);
 }
-template<typename Iterable, typename ...Iterables>
-zipped_iterator<typename Iterable::const_iterator, typename Iterables::const_iterator...>
-zipped_cbegin(const Iterable& arg1, const Iterables&... args) {
-	return zipped_iterator(std::cbegin(arg1), std::cbegin(args)...);
+
+template<typename ...Iterables>
+auto zipped_cbegin(const Iterables&... args) {
+	return zipped_iterator<typename Iterables::const_iterator...>(std::cbegin(args)...);
 }
-template<typename Iterable, typename ...Iterables>
-zipped_iterator<typename Iterable::iterator, typename Iterables::iterator...>
-zipped_end(Iterable& arg1, Iterables&... args) {
-	return zipped_iterator(std::end(arg1), std::end(args)...);
+
+template<typename ...Iterables>
+auto zipped_end(Iterables&... args) {
+	return zipped_iterator<typename Iterables::iterator...>(std::end(args)...);
 }
-template<typename Iterable, typename ...Iterables>
-zipped_iterator<typename Iterable::const_iterator, typename Iterables::const_iterator...>
-zipped_cend(const Iterable& arg1, const Iterables&... args) {
-	return iterator(std::cend(arg1), std::cend(args)...);
+
+template<typename ...Iterables>
+auto zipped_cend(const Iterables&... args) {
+	return zipped_iterator<typename Iterables::const_iterator...>(std::cend(args)...);
 }
 
 template<typename ...Iterables>
