ca993af8e3f2b153c9e46e291f61b628a916a785
FranciscoDA <franciscoda@outlook.com>
2018-08-05 21:09:58 -0300

    add alternated range

---
+++ alternated_range.h
@@ -0,0 +1,82 @@
+#ifndef _ITERUTILS_ALTERNATED_RANGE_H_
+#define _ITERUTILS_ALTERNATED_RANGE_H_
+
+#include <variant>
+#include <array>
+
+#include "detail.h"
+
+template<typename ...Iterators>
+class alternated_iterator {
+public:
+	using value_type = std::common_type_t<Iterators::value_type...>;
+	using reference = std::add_lvalue_reference_t<value_type>;
+	using pointer = std::add_pointer_t<value_type>;
+
+	alternated_iterator(Iterators iterators..., std::size_t index=0) : _its = {iterators...}, _index(index) {
+	}
+	alternated_iterator& operator++() {
+		++_index;
+		return *this;
+	}
+	alternated_iterator operator++(int) {
+		alternated_iterator copy = *this;
+		++(*this);
+		return copy;
+	}
+	reference operator*() {
+		return std::visit([](Iterator&& it){ return *it; }, _its[_index % sizeof...(Iterators)]);
+	}
+	bool operator!=(const alternated_iterator& other) const {
+		return _logical_neq(other, std::index_sequence_for<Iterators...>());
+	}
+private:
+	template<std::size_t ...I>	
+	bool _logical_neq(const alternated_iterator& other, std::index_sequence<I...>) const {
+		return (... && std::get<I>(_its) != std::get<I>(other._its));
+	}
+	std::array<std::variant<Iterators...>, sizeof...(Iterators)> _its;
+	std::size_t _index;
+};
+
+template<typename ...Iterables>
+auto alternated_begin(Iterables&... iterables) {
+	return alternated_iterator<Iterables...>(std::begin(iterables)...);
+}
+
+template<typename ...Iterables>
+auto alternated_end(Iterables&... iterables) {
+	return alternated_iterator<Iterables...>(std::end(iterables)...);
+}
+
+template<typename ...Iterables>
+class alternated_range {
+public:
+	using iterator = alternated_iterator<typename std::remove_reference_t<Iterables>::iterator...>;
+	using value_type = iterator::value_type;
+	using reference = iterator::reference;
+	using pointer = iterator::pointer;
+
+	alternated_range(util::arg_from_uref_t<Iterables>... iterables)
+		: t(iterables) {
+	}
+	iterator begin() { return std::apply(alternated_begin<std::remove_reference_t<Iterables>...>, t); }
+	iterator end() { return std::apply(alternated_end<std::remove_reference_t<Iterables>...>, t); }
+	std::size_t size() const { return _size(); }
+private:
+	template<std::size_t I>
+	std::size_t _size() const {
+		if constexpr (I < sizeof...(Iterables)-1) {
+			return std::min(_element_size<I>(), _element_size<I+1>());
+		}
+		return _element_size<I>();
+	}
+	template<std::size_t I>
+	std::size_t _element_size() const {
+		return std::get<I>(t).size() * sizeof...(Iterables) + I;
+	}
+	std::tuple<Iterables...> t;
+};
+
+#endif
+
